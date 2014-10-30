2e156cb7991109acc69be8adda2f74740f5cca50
kosiacz3q <kosiak.lukasz@gmail.com>
2014-10-30 15:54:48 +0100

    Delete MessagePack.d

--- Debug/src/MessagePack.d
+++
@@ -1,8 +0,0 @@
-src/MessagePack.d: ../src/MessagePack.cpp ../src/MessagePack.h \
- ../src/BrickProperties.h ../src/Vectorf.h
-
-../src/MessagePack.h:
-
-../src/BrickProperties.h:
-
-../src/Vectorf.h:
